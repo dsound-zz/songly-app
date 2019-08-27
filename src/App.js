4ebddc2ead53ff12b7acaf7eaf8feb3851050eda
dsound-zz <demiansims@gmail.com>
2019-08-26 22:13:23 -0400

    08-26-19

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+ 
+ class App extends Component {
+  
+  state = { 
+    value: '',
+    discogTitles: [],
+    musicbrainzTitles: [], 
+    iTunesResults: []
+  };
+
+  handleChange = (event) => {
+   this.setState({ value: event.target.value })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`http://localhost:9000/api/v1/search/${this.state.value}`)
+    .then(res => res.json())
+    .then(results => {
+      this.setState({ iTunesResults: results.iTunes })
+      
+    })
+  }
+
+  // tracksAndArtists = (itunesTitles, itunesArtists) => {
+  //   const tracksArtistList = {}
+  //   itunesTitles.map
+  // }
+  
+  
+  render() {
+    console.log(this.state.iTunesResults) 
+    const { discogsTitles } = this.state
+    return (
+      <>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+            <label>
+              Search: 
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+              <input type="submit" value="Submit" />
+        </form>
+      </div>
+      <div>
+        {discogsTitles && discogsTitles.map(titles => {
+          return <h3>{titles}</h3>
+        })}
+      </div>
+      </>
+    )
+   } 
+} 
+
+export default App; 
+
+ 
+
